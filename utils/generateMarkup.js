7762aabd09c319c2394422ab6c81f1287be2a234
james <james.anthony.russell36@gmail.com>
2022-10-08 18:38:54 +1100

    type for the sting

--- utils/generateMarkup.js
+++ utils/generateMarkup.js
@@ -1,29 +1,38 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// // TODO: Create a function to generate markdown for README
+// function generateMarkdown(data) {
+//   return `# ${data.title}
 
-`;
-}
+// `;
+// }
 
-module.exports = generateMarkdown;
+// module.exports = generateMarkdown;
 
 const fs = require('fs');
 const { type } = require('os');
 
 function generateLicenceBadge() {
 
+
+    // choices: ['mit', 'cc', 'apache', 'none'],
+    let color;
+    if (type === 'MIT') color = 'green';
+    if (type === 'CC') color = 'blue';
+    if (type === 'Apache') color = 'red';
+    if (type === 'None') color = 'orange';
+    
+    return (
 `
 
 ## Licences
@@ -32,7 +41,7 @@ function generateLicenceBadge() {
 
 
 `
-
+    );
 
 }
 
